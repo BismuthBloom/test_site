e301d8ab8e8a579c1e93c105811701990c24e9fa
BismuthBloom <68436600+BismuthBloom@users.noreply.github.com>
2022-11-15 16:55:37 -0500

    Update header.js

--- js/header.js
+++ js/header.js
@@ -1,6 +1,3 @@
-//the header of the site would be handled in this javascript file, so you don't have to copy paste the whole thing onto every page.
-//at the bottom of your page, but before the js script calls and the closing body tag, put an empty div with a class of "writeHeader"
-import { flexNav } from "style.js";
 
 document.querySelector(".writeHeader").innerHTML = `
 
